cc87860b4439e8b6c614212681dde01d4e7fab40
vodnouho <ru.vodnouho@gmail.com>
2016-03-29 12:23:39 +0300

    add localization de, es, fr, ru

--- app/src/main/java/ru/vodnouho/android/atthisdaywidgetapp/LocalizationUtils.java
+++ app/src/main/java/ru/vodnouho/android/atthisdaywidgetapp/LocalizationUtils.java
@@ -1,5 +1,15 @@
 package ru.vodnouho.android.atthisdaywidgetapp;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 /**
  * Created by petukhov on 24.03.2016.
  */
@@ -31,4 +41,93 @@ public class LocalizationUtils {
         }
 
     }
+
+    public static String createLocalizedTitle(Context context, Date date){
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+
+        Resources res = context.getResources();
+        Configuration conf = res.getConfiguration();
+        String lang = conf.locale.getLanguage();
+        String[] monthNames = res.getStringArray(R.array.month_names);
+
+        StringBuilder titleText = new StringBuilder();
+        titleText.append(res.getString(R.string.title));
+
+
+        String localizedMonths = monthNames[month];
+        if (LANG_RU.equals(lang)) {
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+            titleText.append(" ");
+            titleText.append(monthNames[month]);
+        } else if (LANG_EN.equals(lang)) {
+            titleText.append(" ");
+            titleText.append(monthNames[month]);
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+        }else if (LANG_ES.equals(lang)) {
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+            titleText.append(" de ");
+            titleText.append(monthNames[month].toLowerCase());
+
+        }else if (LANG_DE.equals(lang)) {
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+            titleText.append(". ");
+            titleText.append(monthNames[month]);
+/*
+        }else if (FactLab.LANG_ZH.equals(mLang)) { //3月5日
+            return month+"月"+ day + "日";
+*/
+        }else if (LANG_FR.equals(lang)) {
+            String article = " ";
+            if(dayOfMonth == 1){
+                article="er ";
+            }
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+            titleText.append(article);
+            titleText.append(monthNames[month].toLowerCase());
+
+        }else{
+            //like english by default
+            titleText.append(" ");
+            titleText.append(monthNames[month]);
+            titleText.append(" ");
+            titleText.append(dayOfMonth);
+        }
+
+
+        return titleText.toString();
+
+    }
+
+    public static void setLocate(Context context, String lang) {
+        Locale newLocale;
+
+        if (LANG_RU.equals(lang)) {
+            newLocale = new Locale(LANG_RU, "RU");
+        } else if(LANG_ES.equals(lang)) {
+            newLocale = new Locale(LANG_ES, "ES");
+        } else if(LANG_DE.equals(lang)) {
+            newLocale = new Locale(LANG_DE, "DE");
+        } else if(LANG_FR.equals(lang)) {
+                newLocale = new Locale(LANG_FR, "FR");
+        } else {
+            newLocale = Locale.US;
+        }
+
+
+        Resources res = context.getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = newLocale;
+        res.updateConfiguration(conf, dm);
+
+    }
+
 }
--- app/src/main/java/ru/vodnouho/android/atthisdaywidgetapp/OTDWidgetProvider.java
+++ app/src/main/java/ru/vodnouho/android/atthisdaywidgetapp/OTDWidgetProvider.java
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
@@ -22,6 +23,7 @@ import java.util.Locale;
  */
 public class OTDWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "ATDWidgetProvider";
+    private static final String CONTENT_PROVIDER_PACKAGE = "ru.vodnouho.android.yourday";
 
 
     @Override
@@ -51,7 +53,11 @@ public class OTDWidgetProvider extends AppWidgetProvider {
 
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        Log.d(TAG, "updateAppWidget ID:"+appWidgetId);
+        Log.d(TAG, "updateAppWidget ID:" + appWidgetId);
+
+        boolean isContentProviderInstalled  =  isPackageInstalled(CONTENT_PROVIDER_PACKAGE,context);
+        Log.d(TAG, "isContentProviderInstalled:"+isContentProviderInstalled);
+
         //save current Lang
         Resources res = context.getResources();
         Configuration conf = res.getConfiguration();
@@ -81,6 +87,16 @@ public class OTDWidgetProvider extends AppWidgetProvider {
 
     }
 
+    private static boolean isPackageInstalled(String packagename, Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packagename, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
     private static void setTitle(RemoteViews rv, Context context, String settingLang, Date currentDate) {
         //save current Lang
         Resources res = context.getResources();
@@ -92,14 +108,16 @@ public class OTDWidgetProvider extends AppWidgetProvider {
         //get title by setting lang. Use context lang, so synchronize it
         //noinspection SynchronizationOnLocalVariableOrMethodParameter
         synchronized (context){
+            Log.d(TAG, "currentLang:"+currentLang+" settingLang:"+settingLang);
+
             boolean isLangChaged = false;
             if (!settingLang.equals(currentLang)){
-                setLocate(context, settingLang);
+                LocalizationUtils.setLocate(context, settingLang);
                 isLangChaged = true;
             }
             titleText = createTitleText(context, currentDate);
             if(isLangChaged){
-                setLocate(context, currentLang);
+                LocalizationUtils.setLocate(context, currentLang);
             }
         }
 
@@ -142,6 +160,9 @@ public class OTDWidgetProvider extends AppWidgetProvider {
      * @return
      */
     private static String createTitleText(Context context, Date date) {
+        return LocalizationUtils.createLocalizedTitle(context, date);
+
+/*
         // Find current month and day
         Calendar calendar = new GregorianCalendar();
         calendar.setTime(date);
@@ -170,9 +191,11 @@ public class OTDWidgetProvider extends AppWidgetProvider {
 
 
         return titleText.toString();
+*/
     }
 
 
+/*
     private static void setLocate(Context context, String settingLang) {
         Locale newLocale;
         if ("ru".equals(settingLang)) {
@@ -188,6 +211,7 @@ public class OTDWidgetProvider extends AppWidgetProvider {
         res.updateConfiguration(conf, dm);
 
     }
+*/
 
 
 
