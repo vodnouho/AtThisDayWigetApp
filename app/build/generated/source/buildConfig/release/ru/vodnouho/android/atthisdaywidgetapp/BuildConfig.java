cb95afaa63df164e29e234517b36df1bcaad9ef4
vodnouho <ru.vodnouho@gmail.com>
2017-01-22 20:22:22 +0300

    fix colors.xml location

---
+++ app/build/generated/source/buildConfig/release/ru/vodnouho/android/atthisdaywidgetapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.vodnouho.android.atthisdaywidgetapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ru.vodnouho.android.atthisdaywidgetapp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "0.5.6";
+}
